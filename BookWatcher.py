0f4310caab2ecdfd2e1e134e00b2ae84cc29a949
Austin McConnell <austin.s.mcconnell@gmail.com>
2014-08-24 09:36:19 -0500

    Added search result file declaration. Removed example code from module.

--- BookWatcher.py
+++ BookWatcher.py
@@ -4,6 +4,7 @@ from apiclient.discovery import build
 
 bookFile = 'booklist.json'
 keyFile = 'apikeys.json'
+searchFile = 'searchresults.json'
 
 
 def create_keyfile():
@@ -45,7 +46,7 @@ def call_api(request):
     response = request.execute()
 
     # Write request results to json file for debugging/visual inspection
-    with open('results.json', mode='w', encoding='utf-8') as f:
+    with open(searchFile, mode='w', encoding='utf-8') as f:
         json.dump(response, f, indent=4)
 
     return response
@@ -117,31 +118,3 @@ def remove_book(isbn):
 
 def get_amazon_link():
     print('add code here')
-
-
-# Check for apikey file
-if not os.path.isfile(keyFile):
-    create_keyfile()
-    add_api_key('google books')
-
-# Get google books api key
-apiKey_googlebooks = get_api_key('google books')
-
-
-# Add books
-#9780375984648    # The Rule of Thoughts, ISBN13 from BN.com
-#9780316235556    # The Broken Eye, ISBN13 from BN.com
-#9780449818404    # Firefight, ISBN13 from BN.com
-#9781429949620    # Words of Radience, ISBN13 from BN.com
-
-add_book(9780375984648)
-#add_book(9780316235556)
-add_book(9780449818404)
-add_book(9781429949620)
-add_book(9780698184947)
-add_book(9780375984631)
-
-add_book(9780316235556)
-
-# Idea List
-# 3) Search twitter /check author feeds for deals on their books?
